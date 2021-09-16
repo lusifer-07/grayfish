88473acbc21f36eb38b04d6edd5cfe5a2b617350
Lucifermorningstar <83050083+lusifer-07@users.noreply.github.com>
2021-09-16 11:09:15 +0530

    Fuck
    Uuuu

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<?php echo'<script>window.location.replace("./Gray fish/admin/index.php");</script>';?>
+<?php echo'<script>window.location.replace("./Gray fish/admin/index.php");</script>';?
+Fuck off babe>
+It's very easy to make some words **bold** and other words *italic* with Markdown. You can even [link to Google!](http://google.
